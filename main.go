0adc63bad969f92f4225e54d1f41cd44662c977d
Julian Kocon <77498123+JulianKocon@users.noreply.github.com>
2023-03-03 00:51:25 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"example/go-api/initializers"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+}
+
+func main() {
+	fmt.Println("123")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
