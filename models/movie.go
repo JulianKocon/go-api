ab5dec26b9b7a121f57edbb9c2290eb406490671
Julian Kocon <77498123+JulianKocon@users.noreply.github.com>
2023-03-14 19:45:32 +0100

    Add reviewer and genre models

--- models/movie.go
+++ models/movie.go
@@ -7,4 +7,5 @@ type Movie struct {
 	Title       string `json: "title" binding: "required, min=1, max=50" gorm:uniqueIndex`
 	ReleaseYear int    `json: "releaseYear" binding: "required, gte=1894, lte=3000"`
 	Plot        string `json: "plot"`
+	GenreID     uint
 }
